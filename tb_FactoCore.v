0a12734a9c1f4c93f9b98f82f89a1c93b1ffb127
codml <108252974+codml@users.noreply.github.com>
2023-11-28 20:15:24 +0900

    Add files via upload

--- tb_FactoCore.v
+++ tb_FactoCore.v
@@ -13,29 +13,36 @@ module tb_FactoCore;
 		#5 tb_clk = ~tb_clk;
 	end
 	
+	always @(tb_interrupt) begin
+		if (tb_interrupt == 1'b1) begin
+			tb_s_wr <= 1'b0; #10; tb_s_wr <= tb_s_wr <= 1'b1;
+		end
+		else tb_s_wr <= 1'b1;
+	end
+	
 	initial begin
-		tb_clk = 1'b1; tb_reset_n = 1'b0; tb_s_sel = 1'b1; tb_s_wr = 1'b0;
-		tb_s_addr = 16'h7000; tb_s_din = 64'b1; #5;
+		tb_clk = 1'b1; tb_reset_n = 1'b0; tb_s_sel = 1'b1;
+		tb_s_addr = 16'h70ff; tb_s_din = 64'b1; #5;
 		tb_reset_n = 1'b1; #10;
-		tb_s_wr = 1'b1; tb_s_addr = 16'h7020; tb_s_din = 64'd5; #10;
+		tb_s_addr = 16'h7020; tb_s_din = 64'd5; #10;
 		tb_s_addr = 16'h7018; tb_s_din = 64'b1; #10;
-		tb_s_addr = 16'h7000; tb_s_din = 64'b1; #700;
-		tb_s_wr = 1'b0; tb_s_addr = 16'h7030; #100;
-		tb_s_wr = 1'b1; tb_s_addr = 16'h7008; tb_s_din = 64'b1; #10;
+		tb_s_addr = 16'h7000; tb_s_din = 64'b1; #10;
+		tb_s_addr = 16'h7030; #800;
+		tb_s_addr = 16'h7008; tb_s_din = 64'b1; #10;
 		tb_s_addr = 16'h7018; tb_s_din = 64'b1; #10;
 		tb_s_addr = 16'h7020; tb_s_din = 64'd10; #10;
-		tb_s_addr = 16'h7000; tb_s_din = 64'b1; #2000;
-		tb_s_wr = 1'b0; tb_s_addr = 16'h7030; #100;
-		tb_s_wr = 1'b1; tb_s_addr = 16'h7008; tb_s_din = 64'b1; #10;
+		tb_s_addr = 16'h7000; tb_s_din = 64'b1; #10;
+		tb_s_addr = 16'h7030; #1700;
+		tb_s_addr = 16'h7008; tb_s_din = 64'b1; #10;
 		tb_s_addr = 16'h7018; tb_s_din = 64'b1; #10;
 		tb_s_addr = 16'h7020; tb_s_din = 64'b0; #10;
-		tb_s_addr = 16'h7000; tb_s_din = 64'b1; #100;
-		tb_s_wr = 1'b0; tb_s_addr = 16'h7030; #100;
-		tb_s_wr = 1'b1; tb_s_addr = 16'h7008; tb_s_din = 64'b1; #10;
+		tb_s_addr = 16'h7000; tb_s_din = 64'b1; #10;
+		tb_s_addr = 16'h7030; #100;
+		tb_s_addr = 16'h7008; tb_s_din = 64'b1; #10;
 		tb_s_addr = 16'h7018; tb_s_din = 64'b1; #10;
 		tb_s_addr = 16'h7020; tb_s_din = 64'b1; #10;
-		tb_s_addr = 16'h7000; tb_s_din = 64'b1; #100;
-		tb_s_wr = 1'b0; tb_s_addr = 16'h7030; #100;
+		tb_s_addr = 16'h7000; tb_s_din = 64'b1; #10;
+		tb_s_addr = 16'h7030; #100;
 		$stop;
 	end
 endmodule
